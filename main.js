4b1bc80834a3aaf5ada71a34d5646db8805186a5
Nemo080 <nishatf01@gmail.com>
2024-12-18 00:31:43 -0500

    await function edited to async function

--- main.js
+++ main.js
@@ -1,84 +1,83 @@
-import './style.css'
-import  fetchAnime  from './api-call.js'
+import './style.css';
+import fetchAnime from './api-call.js';
 
-const data = await fetchAnime()
-const animeArray = data.data
+async function main() {
+  try {
+    const data = await fetchAnime();
+    const animeArray = data.data;
 
-function display(animeArray){
+    function display(animeArray) {
+      // Sorting the animeArray in reverse alphabetical order to get the latest item
+      let sortedAnime = animeArray.sort((a, b) => {
+        let current = a.title;
+        let latest = b.title;
+        if (current < latest) {
+          return 1;
+        } else if (current > latest) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      console.log(sortedAnime);
 
-  // sorting the animeArray in reverse alphabetical order to get the latest item
-  let sortedAnime = animeArray.sort((a, b) => {
-    let current = a.title;
-    let latest = b.title;
-    if (current < latest){
-      return 1;
-    }
-    else if (current > latest) {
-      return - 1;
-    }
-    else {
-      return 0;
-    }
-  })
-  console.log(sortedAnime)
-  
+      // Filtering the anime to show data from after 2022
+      let filteredArray = sortedAnime.filter((anime) => {
+        return anime.aired && anime.aired.from && anime.aired.from.slice(0, 4) >= "2022";
+      });
+      console.log(filteredArray);
 
-  // filtering the anime to show data from after 2022
-  let filteredArray = sortedAnime.filter((anime) => {
-    return anime.aired && anime.aired.from && anime.aired.from.slice(0, 4) >= "2022"
-    
-  })  
-  console.log(filteredArray)
+      filteredArray = filteredArray.slice(1);
 
-  filteredArray = filteredArray.slice(1);
-  
-  // appending the title and the image of the anime on the body of the browser with the use of map(). map() creates a new array with out altering the original array
-  filteredArray.map((anime) => {
+      // Appending the title and the image of the anime on the body of the browser
+      filteredArray.map((anime) => {
+        const animeContainer = document.createElement('div');
+        animeContainer.className = 'anime-container';
 
-    const animeContainer = document.createElement('div');
-    animeContainer.className = 'anime-container';
+        const infoContainer = document.createElement('div');
+        infoContainer.className = 'info-container';
 
-    const infoContainer = document.createElement('div');
-    infoContainer.className = 'info-container';
+        const newImg = document.createElement('img');
+        newImg.src = anime.images.jpg.image_url;
+        newImg.className = 'anime-image';
 
-    const newImg = document.createElement('img');
-    newImg.src = anime.images.jpg.image_url;
-    newImg.className = 'anime-image';
+        const newTitle = document.createElement('h2');
+        newTitle.innerText = anime.title;
+        newTitle.className = 'anime-title';
 
-    const newTitle = document.createElement('h2');
-    newTitle.innerText = anime.title;
-    newTitle.className = 'anime-title';
-
-    const button = document.createElement('button');
-    button.innerText = 'Show Description';
-    button.className = 'anime-button';
-
-    // Add a click event to the button to toggle the description
-    button.addEventListener('click', () => {
-      if (!animeContainer.querySelector('.description')) {
-        const description = document.createElement('p');
-        description.innerText = anime.synopsis;
-        description.className = 'description'; // Class for easy reference
-        animeContainer.appendChild(description);
-        button.innerText = 'Hide Description';
-      } else {
-        const description = animeContainer.querySelector('.description');
-        description.remove(); // Remove the description from the DOM
+        const button = document.createElement('button');
         button.innerText = 'Show Description';
-      }
+        button.className = 'anime-button';
 
-    });
-    
-    // imgContainer.appendChild(newImg);
-    infoContainer.append(newTitle, button);
-    animeContainer.append(infoContainer);
-    document.body.append(animeContainer);
+        // Add a click event to the button to toggle the description
+        button.addEventListener('click', () => {
+          if (!animeContainer.querySelector('.description')) {
+            const description = document.createElement('p');
+            description.innerText = anime.synopsis;
+            description.className = 'description'; // Class for easy reference
+            animeContainer.appendChild(description);
+            button.innerText = 'Hide Description';
+          } else {
+            const description = animeContainer.querySelector('.description');
+            description.remove(); // Remove the description from the DOM
+            button.innerText = 'Show Description';
+          }
+        });
 
-    animeContainer.append(newTitle, newImg, button);
-    document.body.append(animeContainer);
-  });
+        infoContainer.append(newTitle, button);
+        animeContainer.append(infoContainer);
+        animeContainer.append(newTitle, newImg, button);
+        document.body.append(animeContainer);
+      });
 
-  document.body.style.textAlign = "center";
+      document.body.style.textAlign = "center";
+    }
+
+    display(animeArray);
+  } catch (error) {
+    console.error("Error fetching anime data:", error);
+  }
 }
 
-display(animeArray)
+// Call the async function
+main();
